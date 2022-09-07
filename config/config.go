6b26859689a45729df2103c003c25fb0d172bf68
saeed hamzenezhad <saeed.0347@gmail.com>
2022-09-07 10:51:20 +0430

    Changing the database package

--- config/config.go
+++ config/config.go
@@ -2,16 +2,35 @@ package config
 
 import (
 	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
+type config struct{
+	DbConfig	databse
+}
 
+type databse struct{
+	Port		string
+	Host		string
+	User		string
+	Password	string
+	Name		string
+	TimeZone	string
+}
+
+var AppConfig config
 
 func SetConfig(){
-
 	err := godotenv.Load("./config/.env")
 	if err!=nil{
 		log.Fatalf("Some error occured. Err %s",err)
 	}
-
+	AppConfig.DbConfig.Host = os.Getenv("POSTGRES_HOST")
+	AppConfig.DbConfig.Port = os.Getenv("POSTGRES_PORT")
+	AppConfig.DbConfig.User = os.Getenv("POSTGRES_USER")
+	AppConfig.DbConfig.Password = os.Getenv("POSTGRES_PASSWORD")
+	AppConfig.DbConfig.Name = os.Getenv("POSTGRES_NAME")
+	AppConfig.DbConfig.TimeZone = os.Getenv("POSTGRES_TIMEZONE")
 }
--- database/database.go
+++ database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"github.com/uncodev/educational-website-with-golang/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"sync"
 )
 
 
@@ -12,13 +15,25 @@ type Connection struct{
 }
 
 
-var Conn Connection
+var GetConnection Connection
 
-func Connect(){
-	dsn := "postgresql://postgres:postgres@db:5432/postgres?sslmode=disable"
-	db , err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
-	if err!=nil {
-		log.Fatal(err)
-	}
-	Conn.Conn = db	
+var doOnce sync.Once
+
+func Connect() *gorm.DB {
+	doOnce.Do(func() {
+		dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v timezone=%v",
+			config.AppConfig.DbConfig.Host,
+			config.AppConfig.DbConfig.User,
+			config.AppConfig.DbConfig.Password,
+			config.AppConfig.DbConfig.Name,
+			config.AppConfig.DbConfig.TimeZone,
+		)
+		db , err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+		if err!=nil {
+			log.Fatal(err)
+		}
+		GetConnection.Conn = db
+	})
+	return GetConnection.Conn
 }
+
